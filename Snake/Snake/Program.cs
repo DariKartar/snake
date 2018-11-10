13a72aaa2acc50a890054a12d3309691ef3984f0
DariKartat <darikartar@gmail.com>
2018-11-10 23:05:39 +0300

    learn list

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,16 +13,34 @@ namespace Snake
         {
 
             Point p1 = new Point(1, 3, '*');
-            //p1.x = 1;
-            //p1.y = 3;
-            //p1.sym = '*';
-
+          
             p1.Draw();
 
 
             Point p2 = new Point(6, 8, '$');
           
             p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+             
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pointList = new List<Point>();
+            pointList.Add(p1);
+            pointList.Add(p2);
 
 
             Console.ReadLine();
